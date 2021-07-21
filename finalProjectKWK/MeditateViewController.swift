827250529569d36e3937344fae50715e89ad7278
raviry <ravi_ry@yahoo.com>
2021-07-21 11:35:04 -0700

    code! meditate button!

--- finalProjectKWK/MeditateViewController.swift
+++ finalProjectKWK/MeditateViewController.swift
@@ -9,21 +9,40 @@ import UIKit
 
 class MeditateViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    var timer = Timer()
+    var seconds = 300
+    
+    @IBAction func startButton(_ sender: Any) {
+        // no other timers running
+        timer.invalidate()
+        // create timer
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(MeditateViewController.timerClass), userInfo: nil, repeats: true)
+    }
+
+    @IBAction func pauseButton(_ sender: Any) {
+        timer.invalidate()
+    }
+    @IBAction func resetButton(_ sender: Any) {
+        timer.invalidate()
+        seconds = 300
+        timerLabel.text = String(seconds)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func timerClass(){
+        seconds -= 1
+        timerLabel.text = String(seconds)
+        
+        if(seconds == 0) {
+            timer.invalidate()
+        }
     }
-    */
 
 }
