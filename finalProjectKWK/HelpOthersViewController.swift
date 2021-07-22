7f5935b297c94be94836c17397682b93e356a0d3
Nicole Bond <nicolebond2263@gmail.com>
2021-07-22 01:08:43 +0100

    Started iteration 1 but there's a problem with the help others section

--- finalProjectKWK/HelpOthersViewController.swift
+++ finalProjectKWK/HelpOthersViewController.swift
@@ -8,14 +8,26 @@
 import UIKit
 
 class HelpOthersViewController: UIViewController {
-
+    var previousVC = ReadKindMessagesTableViewController()
+    
+    @IBOutlet weak var leaveKindMessagesBox: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func submitTapped(_ sender: Any) {
+        let kindMessage = KindMessage()
+        
+        if let leaveKindMessage = leaveKindMessagesBox.text {
+            kindMessage.message = leaveKindMessage
+        }
+        
+        previousVC.kindMessages.append(kindMessage)
+        previousVC.tableView.reloadData()
+        
+    }
+    
     /*
     // MARK: - Navigation
 
